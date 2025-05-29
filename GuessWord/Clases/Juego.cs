fd7314dde1060a4c72c0bd69c2b49f139b2efcc2
Herberth <herberthmamani761@gmail.com>
2025-05-29 10:26:52 -0400

    Desarrolle un poco la clase juego y ademas hice una clase aparte para funcionalidades que posiblemente utilicemos

---
+++ GuessWord/Clases/Juego.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessWord.Clases
+{
+    public class Juego
+    {
+        public char[] SecretWord {get; set; }
+        public int Attempts { get; set; }
+        public int MaxAttempts { get; set; }
+        public Juego()
+        {
+            string palabra = Recursos.listadepalabras[new Random().Next(Recursos.listadepalabras.Count)];
+            SecretWord = palabra.ToCharArray();
+            MaxAttempts = 5;
+            Attempts = 0;
+            Recursos.Historial.Add(palabra);
+        }
+        public bool IsGameOver()
+        {
+            return Attempts >= MaxAttempts;
+        }
+        
+    }
+}
---
+++ GuessWord/Clases/Recursos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GuessWord.Clases
+{
+    public static class Recursos
+    {
+        public static List<string> listadepalabras;
+        static string ruta = "palabras.txt";
+        public static List<string> Historial;
+
+        public static void InicializarPalabras()
+        {
+            listadepalabras = CargarPalabras();
+        }
+        private static List<string> CargarPalabras()
+        {
+            if (!File.Exists(Recursos.ruta))
+            {
+                return new List<string>();
+            }
+            return File.ReadAllLines(Recursos.ruta).Where(lineas => !string.IsNullOrWhiteSpace(lineas)).Select(lineas => lineas.Trim()).ToList();
+        }
+    }
+}
